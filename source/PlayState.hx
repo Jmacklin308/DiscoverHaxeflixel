089804d628703fa5368bbb7052d9f2ee0f150fb7
Justin Macklin <Jmacklin308>
2021-04-17 01:09:05 -0500

    Initial commit

---
+++ source/PlayState.hx
@@ -0,0 +1,50 @@
+/*
+	This is the first game "scene" (what FlxState's are)
+ */
+
+package;
+
+import flixel.FlxG;
+import flixel.FlxObject; // Contains some collision logic
+import flixel.FlxSprite;
+import flixel.FlxState;
+import flixel.math.FlxMath;
+import flixel.system.macros.FlxAssetPaths;
+import flixel.text.FlxText;
+import flixel.tile.FlxTilemap; // allow us to build a level
+import flixel.ui.FlxButton;
+
+// import flixel.system.macros.FlxAssetPaths;
+class PlayState extends FlxState
+{
+	var map:FlxTilemap;
+	var player:FlxSprite;
+	var mapData:Array<Int> = [
+		1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+		1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+		1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+		1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1,
+		1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1,
+		1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+		1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+		1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+		1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+		1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+		1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+		1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+	];
+
+	// Create function - called once at beginning of scene
+	override public function create()
+	{
+		// create a map
+
+		super.create();
+	};
+
+	// Update function - called every frame
+	override public function update(elapsed:Float)
+	{
+		super.update(elapsed);
+	}
+}
